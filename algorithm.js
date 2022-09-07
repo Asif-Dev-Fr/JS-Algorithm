8bb3aedeaf276671543909cdba51b3b8be24b562
Asif Kassam Ali <asif.k.dev@gmail.com>
2022-09-07 17:42:28 +0200

    algo added

--- algorithm.js
+++ algorithm.js
@@ -592,3 +592,32 @@ const pairOfShoes = (shoes) =>
       {}
     )
   ).every((val) => !val);
+
+/* --------------------------------------------------------------------------------------------------------------------------- */
+
+/*
+  ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+
+  If the function is passed a valid PIN string, return true, else return false.
+
+  Examples (Input --> Output)
+  "1234"   -->  true
+  "12345"  -->  false
+  "a234"   -->  false
+*/
+
+// My code
+function areYouPlayingBanjo(name) {
+  let split = name.toLowerCase().split("");
+  if (split[0] === "r") return name + " plays banjo";
+  else return name + " does not play banjo";
+}
+
+// Best practice
+function areYouPlayingBanjo(name) {
+  return (
+    name +
+    (name[0].toLowerCase() == "r" ? " plays" : " does not play") +
+    " banjo"
+  );
+}
