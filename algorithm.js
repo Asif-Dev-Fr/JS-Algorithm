8984737e4df282642da872fd2f56a86f38786140
Asif Kassam Ali <asif.k.dev@gmail.com>
2022-11-20 18:55:19 +0100

    new add

--- algorithm.js
+++ algorithm.js
@@ -82,6 +82,12 @@ function digitize(n) {
   return array;
 }
 
+// Better :
+function digitize(n) {
+  let reverse = n.toString().split("").reverse().join("");
+  return parseInt(reverse);
+}
+
 /* --------------------------------------------------------------------------------------------------------------------------- */
 
 /*
@@ -860,4 +866,96 @@ function minMax(arr) {
 // Best pratice
 function minMax(arr) {
   return [Math.min(...arr), Math.max(...arr)];
+}
+
+/* --------------------------------------------------------------------------------------------------------------------------- */
+
+/*
+  DESCRIPTION:
+  Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+
+  For example:
+
+  solution([1, 2, 10, 50, 5]); // should return [1,2,5,10,50]
+  solution(null); // should return []
+*/
+
+// My code
+function solution(nums) {
+  return (nums && nums.length === 0) || nums === null
+    ? []
+    : nums.sort((a, b) => a - b);
+}
+
+// Best pratice
+function solution(nums) {
+  return nums !== null
+    ? nums.sort(function (a, b) {
+        return a - b;
+      })
+    : [];
+}
+
+/* --------------------------------------------------------------------------------------------------------------------------- */
+
+/*
+  In democracy we have a lot of elections. For example, we have to vote for a class representative in school, for a new parliament or a new government.
+
+  Usually, we vote for a candidate, i.e. a set of eligible candidates is given. This is done by casting a ballot into a ballot-box. After that, it must be counted how many ballots (= votes) a candidate got.
+
+  A candidate will win this election if he has the absolute majority.
+
+  Your Task
+  Return the name of the winner. If there is no winner, return null / nil / None depending on the language.
+
+  Task Description
+  There are no given candidates. An elector can vote for anyone. Each ballot contains only one name and represents one vote for this name. A name is an arbitrary string, e.g. "A", "B", or "XJDHD". There are no spoiled ballots.
+
+  The ballot-box is represented by an unsorted list of names. Each entry in the list corresponds to one vote for this name. You do not know the names in advance (because there are no candidates).
+
+  A name wins the election if it gets more than n / 2 votes (n = number of all votes, i.e. the size of the given list).
+
+  Examples
+  # 3 votes for "A", 2 votes for "B"  -->  "A" wins the election
+  ["A", "A", "A", "B", "B"]  -->  "A"
+
+  # 2 votes for "A", 2 votes for "B"  -->  no winner
+  ["A", "A", "B", "B"]  -->  None / nil / null
+
+  # 1 vote for each name --> no winner
+  ["A", "B", "C", "D"]  -->  None / nil / null
+
+  # 3 votes for "A", 2 votes for "B", 1 vote for "C"  -->  no winner, as "A" does not have more than n / 2 = 3 votes
+  ["A", "A", "A", "B", "B", "C"] -->  None / nil / null
+  Note
+  Please keep in mind the list of votes can be large (n <= 1 200 000). The given list is immutable, i.e. you cannot modify the list (otherwise this could lead to vote rigging).
+
+  Good luck and have fun.
+*/
+
+// My code
+function getWinner(listOfBallots) {
+  //Your code here
+  let obj = {};
+  let count = 0;
+  let winner = "";
+  let totalCount = 0;
+  for (let candidate of listOfBallots) {
+    if (obj[candidate]) {
+      obj[candidate] += 1;
+      totalCount = totalCount + 1;
+    } else {
+      obj[candidate] = 1;
+      totalCount = totalCount + 1;
+    }
+  }
+  for (candidate in obj) {
+    if (count < obj[candidate]) {
+      count = obj[candidate];
+      winner = candidate;
+    }
+  }
+
+  if (totalCount / 2 < count) return winner;
+  else return null;
 }
