cf49deec51f87d6455e5f210079dc1daaab413e7
Asif Kassam Ali <asif.k.dev@gmail.com>
2022-12-01 17:53:20 +0100

    algo added

--- algorithm.js
+++ algorithm.js
@@ -1853,3 +1853,51 @@ function past(h, m, s) {
 function past(h, m, s) {
   return (h * 3600 + m * 60 + s) * 1000;
 }
+
+/* --------------------------------------------------------------------------------------------------------------------------- */
+
+/*
+  DESCRIPTION:
+  Task
+  You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+  Examples
+  [7, 1]  =>  [1, 7]
+  [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+  [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+*/
+
+// My code
+function sortArray(array) {
+  const odds = array.filter((x) => x % 2).sort((a, b) => a - b);
+  return array.map((x) => (x % 2 ? odds.shift() : x));
+}
+
+// Alternative
+function sortArray(array) {
+  var odds = [];
+  //loop, if it's odd, push to odds array
+  for (var i = 0; i < array.length; ++i) {
+    if (array[i] % 2 !== 0) {
+      odds.push(array[i]);
+    }
+  }
+  //sort odds from smallest to largest
+  odds.sort(function (a, b) {
+    return a - b;
+  });
+
+  //loop through array, replace any odd values with sorted odd values
+  for (var j = 0; j < array.length; ++j) {
+    if (array[j] % 2 !== 0) {
+      array[j] = odds.shift();
+    }
+  }
+
+  return array;
+}
+
+function sortArray(array) {
+  var odd = array.filter((elem) => elem % 2 !== 0).sort((a, b) => a - b);
+  return array.map((elem) => (elem % 2 === 0 ? elem : odd.shift()));
+}
