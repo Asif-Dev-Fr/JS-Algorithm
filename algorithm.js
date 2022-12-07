437eda834df9e8405f0771c9cee7e24e59853704
Asif Kassam Ali <asif.k.dev@gmail.com>
2022-12-07 17:24:54 +0100

    algo added

--- algorithm.js
+++ algorithm.js
@@ -2112,3 +2112,44 @@ function reverseWords(str) {
   //split words into seperate arrays
   return str.split("").reverse().join("").split(" ").reverse().join(" ");
 }
+
+/* --------------------------------------------------------------------------------------------------------------------------- */
+
+/*
+  DESCRIPTION:
+  In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+  Example
+  filter_list([1,2,'a','b']) == [1,2]
+  filter_list([1,'a','b',0,15]) == [1,0,15]
+  filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+*/
+
+// My code
+function filter_list(l) {
+  return l.filter((n) => typeof n !== "string");
+}
+
+/* --------------------------------------------------------------------------------------------------------------------------- */
+
+/*
+  DESCRIPTION:
+  Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+*/
+
+// My code
+function removeExclamationMarks(s) {
+  return s
+    .split("")
+    .filter((v) => v !== "!")
+    .join("");
+}
+
+// Alternative
+function removeExclamationMarks(s) {
+  return s.replace(/!/gi, "");
+}
+
+function removeExclamationMarks(s) {
+  return s.split("!").join("");
+}
