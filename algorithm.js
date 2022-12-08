f8dd9215f6110f024a05417a5cc072a119417f21
Asif Kassam Ali <asif.k.dev@gmail.com>
2022-12-08 17:54:48 +0100

    algo added

--- algorithm.js
+++ algorithm.js
@@ -2153,3 +2153,45 @@ function removeExclamationMarks(s) {
 function removeExclamationMarks(s) {
   return s.split("!").join("");
 }
+
+/* --------------------------------------------------------------------------------------------------------------------------- */
+
+/*
+  DESCRIPTION:
+  Given a string of words, you need to find the highest scoring word.
+
+  Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+
+  You need to return the highest scoring word as a string.
+
+  If two words score the same, return the word that appears earliest in the original string.
+
+  All letters will be lowercase and all inputs will be valid.
+*/
+
+// My code
+function high(x) {
+  let alphabet = "abcdefghijklmnopqrstuvwxyz";
+  let words = x.split(" ");
+  let best = "";
+  let maxscore = 0;
+  for (let word of words) {
+    let score = 0;
+    for (let letter of word) {
+      score += alphabet.indexOf(letter) + 1;
+    }
+    if (maxscore < score) {
+      best = word;
+      maxscore = score;
+    }
+  }
+  return best;
+}
+
+// Best pratices
+function high(s) {
+  let as = s
+    .split(" ")
+    .map((s) => [...s].reduce((a, b) => a + b.charCodeAt(0) - 96, 0));
+  return s.split(" ")[as.indexOf(Math.max(...as))];
+}
