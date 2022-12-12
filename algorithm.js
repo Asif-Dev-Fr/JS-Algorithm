bb92a73fabc81049b9a089469f9a8fb5affd9499
Asif Kassam Ali <asif.k.dev@gmail.com>
2022-12-12 18:00:11 +0100

    algo added

--- algorithm.js
+++ algorithm.js
@@ -2195,3 +2195,62 @@ function high(s) {
     .map((s) => [...s].reduce((a, b) => a + b.charCodeAt(0) - 96, 0));
   return s.split(" ")[as.indexOf(Math.max(...as))];
 }
+
+/* --------------------------------------------------------------------------------------------------------------------------- */
+
+/*
+  DESCRIPTION:
+  Complete the solution so that the function will break up camel casing, using a space between words.
+
+  Example
+  "camelCasing"  =>  "camel Casing"
+  "identifier"   =>  "identifier"
+  ""             =>  ""
+*/
+
+// My code
+function solution(string) {
+  return string.replace(/([A-Z])/g, " $1");
+}
+
+// Alternative
+function solution(string) {
+  string = string.split("").map((el) => {
+    if (el === el.toUpperCase()) {
+      el = " " + el;
+    }
+    return el;
+  });
+  return string.join("");
+}
+
+/* --------------------------------------------------------------------------------------------------------------------------- */
+
+/*
+  DESCRIPTION:
+  The museum of incredible dull things
+  The museum of incredible dull things wants to get rid of some exhibitions. Miriam, the interior architect, comes up with a plan to remove the most boring exhibitions. She gives them a rating, and then removes the one with the lowest rating.
+
+  However, just as she finished rating all exhibitions, she's off to an important fair, so she asks you to write a program that tells her the ratings of the items after one removed the lowest one. Fair enough.
+
+  Task
+  Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
+
+  Don't change the order of the elements that are left.
+
+  Examples
+  * Input: [1,2,3,4,5], output = [2,3,4,5]
+  * Input: [5,3,2,1,4], output = [5,3,2,4]
+  * Input: [2,2,1,2,1], output = [2,2,2,1]
+*/
+
+function removeSmallest(numbers) {
+  const duplicate = numbers;
+  let smallestValue = numbers.indexOf(Math.min(...numbers));
+  duplicate.splice(smallestValue, 1);
+  return duplicate;
+}
+
+// Best pratices
+const removeSmallest = (numbers) =>
+  numbers.filter((n, i) => i !== numbers.indexOf(Math.min(...numbers)));
