620ac95f61130475042230482ed67375e7ce099a
Asif Kassam Ali <asif.k.dev@gmail.com>
2022-12-21 17:58:53 +0100

    algo added + exo to finish

--- algorithm.js
+++ algorithm.js
@@ -2696,3 +2696,63 @@ function likes(names) {
       );
   }
 }
+
+/* --------------------------------------------------------------------------------------------------------------------------- */
+
+/*
+  DESCRIPTION:
+  Count the number of divisors of a positive integer n.
+
+  Random tests go up to n = 500000.
+
+  Examples (input --> output)
+  4 --> 3 (1, 2, 4)
+  5 --> 2 (1, 5)
+  12 --> 6 (1, 2, 3, 4, 6, 12)
+  30 --> 8 (1, 2, 3, 5, 6, 10, 15, 30)
+  Note you should only return a number, the count of divisors. The numbers between parentheses are shown only for you to see which numbers are counted in each case.
+*/
+
+// My code
+function getDivisorsCnt(n) {
+  let count = 0;
+  for (let i = 0; i <= n; i++) {
+    if (Number.isInteger(n / i)) {
+      count++;
+    }
+  }
+  return count;
+}
+
+// Alternative
+function getDivisorsCnt(n) {
+  var cnt = 1;
+  for (var i = 0; i <= n / 2; ++i) {
+    if (n % i == 0) ++cnt;
+  }
+  return cnt;
+}
+
+/* --------------------------------------------------------------------------------------------------------------------------- */
+
+/*
+  DESCRIPTION:
+  A stream of data is received and needs to be reversed.
+
+  Each segment is 8 bits long, meaning the order of these segments needs to be reversed, for example:
+
+  11111111  00000000  00001111  10101010
+  (byte1)   (byte2)   (byte3)   (byte4)
+  should become:
+
+  10101010  00001111  00000000  11111111
+  (byte4)   (byte3)   (byte2)   (byte1)
+  The total number of bits will always be a multiple of 8.
+
+  The data is given in an array as such:
+
+  [1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0]
+*/
+
+// My code
+// https://www.codewars.com/kata/569d488d61b812a0f7000015/train/javascript
