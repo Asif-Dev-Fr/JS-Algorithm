93188ff36c5ee051cdd2fdcc6a5ec6bcb6d2f94a
Asif Kassam Ali <asif.k.dev@gmail.com>
2022-12-30 17:48:23 +0100

    algo added

--- algorithm.js
+++ algorithm.js
@@ -3176,3 +3176,88 @@ const findMissingLetter = (array) => {
     .slice(start, start + array.length)
     .find((el) => !array.includes(el));
 };
+
+/* --------------------------------------------------------------------------------------------------------------------------- */
+
+/*
+  DESCRIPTION:
+  Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
+
+  Rules for a smiling face:
+
+  Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+  A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+  Every smiling face must have a smiling mouth that should be marked with either ) or D
+  No additional characters are allowed except for those mentioned.
+
+  Valid smiley face examples: :) :D ;-D :~)
+  Invalid smiley faces: ;( :> :} :]
+
+  Example
+  countSmileys([':)', ';(', ';}', ':-D']);       // should return 2;
+  countSmileys([';D', ':-(', ':-)', ';~)']);     // should return 3;
+  countSmileys([';]', ':[', ';*', ':$', ';-D']); // should return 1;
+  Note
+  In case of an empty array return 0. You will not be tested with invalid input (input will always be an array). Order of the face (eyes, nose, mouth) elements will always be the same.
+*/
+
+// My code
+function countSmileys(arr) {
+  if (arr.length === 0) return 0;
+
+  return arr.filter((x) => {
+    return x.match(/[:;][-~]?[D)]/g);
+  }).length;
+}
+
+// Alternative
+function countSmileys(arr) {
+  return arr.filter((x) => /^[:;][-~]?[)D]$/.test(x)).length;
+}
+
+/* --------------------------------------------------------------------------------------------------------------------------- */
+
+/*
+  DESCRIPTION:
+  The marketing team is spending way too much time typing in hashtags.
+  Let's help them with our own Hashtag Generator!
+
+  Here's the deal:
+
+  It must start with a hashtag (#).
+  All words must have their first letter capitalized.
+  If the final result is longer than 140 chars it must return false.
+  If the input or the result is an empty string it must return false.
+  Examples
+  " Hello there thanks for trying my Kata"  =>  "#HelloThereThanksForTryingMyKata"
+  "    Hello     World   "                  =>  "#HelloWorld"
+  ""                                        =>  false
+*/
+
+// My code
+function generateHashtag(str) {
+  if (str.length === 0) return false;
+
+  let newStr = "#";
+  for (let word of str.split(" ")) {
+    if (word && word !== " ") {
+      newStr += word[0].toUpperCase() + word.slice(1);
+    }
+  }
+  return newStr.length > 1 && newStr.length <= 140 ? newStr : false;
+}
+
+// Alternative
+function generateHashtag(str) {
+  if (!str || str.length < 1) return false;
+
+  var r =
+    "#" +
+    str
+      .split(" ")
+      .map(function (el) {
+        return el.charAt(0).toUpperCase() + el.slice(1).toLowerCase();
+      })
+      .join("");
+  return r.length > 140 ? false : r;
+}
