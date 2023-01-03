5b92f6f7abe9bb66d02f67ccc77f1169fdf49094
Asif Kassam Ali <asif.k.dev@gmail.com>
2023-01-03 18:02:11 +0100

    algo added

--- algorithm.js
+++ algorithm.js
@@ -3380,3 +3380,59 @@ function sumStr(a, b) {
 }
 // Coding +'' after an integer turns it into a string. This integer is the sum of two integers; each of which got converted from their string by the use of the + in front of them.
 
+/* --------------------------------------------------------------------------------------------------------------------------- */
+
+/*
+  DESCRIPTION:
+  There was a test in your class and you passed it. Congratulations!
+  But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+  You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+  Return True if you're better, else False!
+
+  Note:
+  Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
+*/
+
+// My code
+function betterThanAverage(classPoints, yourPoints) {
+  classPoints.push(yourPoints);
+  return (
+    classPoints.reduce((a, b) => a + b, 0) / classPoints.length < yourPoints
+  );
+}
+
+/* --------------------------------------------------------------------------------------------------------------------------- */
+
+/*
+  DESCRIPTION:
+  Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+  Examples (Input -> Output):
+  * "String"      -> "SSttrriinngg"
+  * "Hello World" -> "HHeelllloo  WWoorrlldd"
+  * "1234!_ "     -> "11223344!!__  "
+*/
+
+// My code
+function doubleChar(str) {
+  return str
+    .split("")
+    .map((l) => {
+      return l.repeat(2);
+    })
+    .join("");
+}
+
+// Alternative
+const doubleChar = (str) => {
+  str
+    .split("")
+    .map((c) => c + c)
+    .join("");
+};
+
+function doubleChar(str) {
+  return str.replace(/(.)/g, "$1$1");
+}
