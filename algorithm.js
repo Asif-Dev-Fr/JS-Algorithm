f0f45e6bdfe9a3b1e5093457cef2cd02f2f8e47c
Asif Kassam Ali <asif.k.dev@gmail.com>
2023-01-09 18:24:51 +0100

    algo added

--- algorithm.js
+++ algorithm.js
@@ -3747,3 +3747,45 @@ function bouncingBall(h, bounce, window) {
     while (h > window) (rebounds += 2), (h *= bounce);
   return rebounds;
 }
+
+/* --------------------------------------------------------------------------------------------------------------------------- */
+
+/*
+  DESCRIPTION:
+  Complete the function that counts the number of unique consonants in a string (made up of printable ascii characters).
+
+  Consonants are letters used in English other than "a", "e", "i", "o", "u". We will count "y" as a consonant.
+
+  Remember, your function needs to return the number of unique consonants - disregarding duplicates. For example, if the string passed into the function reads "add", the function should return 1 rather than 2, since "d" is a duplicate.
+
+  Similarly, the function should also disregard duplicate consonants of differing cases. For example, "Dad" passed into the function should return 1 as "d" and "D" are duplicates.
+
+  Examples
+  "add" ==> 1
+  "Dad" ==> 1
+  "aeiou" ==> 0
+  "sillystring" ==> 7
+  "abcdefghijklmnopqrstuvwxyz" ==> 21
+  "Count my unique consonants!!" ==> 7
+*/
+
+// My code
+function countConsonants(str) {
+  let vowel = ["a", "e", "i", "o", "u"];
+  let arr = [];
+  for (let l of str.split("")) {
+    if (
+      !vowel.includes(l.toLowerCase()) &&
+      !arr.includes(l.toLowerCase()) &&
+      l.match(/[a-zA-Z]/g)
+    ) {
+      arr.push(l.toLowerCase());
+    }
+  }
+  return arr.length;
+}
+
+// Alternative
+function countConsonants(str) {
+  return new Set(str.toLowerCase().replace(/[^a-z]|[aeiou]/g, "")).size;
+}
