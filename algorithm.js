47df4a63039e9cc3f74a987c0f6bd08c7b5d5ba3
Asif Kassam Ali <asif.k.dev@gmail.com>
2023-01-16 18:05:53 +0100

    algo added

--- algorithm.js
+++ algorithm.js
@@ -4176,3 +4176,67 @@ function wave(str) {
   }
   return newArr;
 }
+
+/* ---------------------------------------------------------------------------------------------------------------------------*/
+
+/*
+  DESCRIPTION:
+  A string is considered to be in title case if each word in the string is either (a) capitalised (that is, only the first letter of the word is in upper case) or (b) considered to be an exception and put entirely into lower case unless it is the first word, which is always capitalised.
+
+  Write a function that will convert a string into title case, given an optional list of exceptions (minor words). The list of minor words will be given as a string with each word separated by a space. Your function should ignore the case of the minor words string -- it should behave in the same way even if the case of the minor word string is changed.
+
+  Arguments 
+  First argument (required): the original string to be converted.
+  Second argument (optional): space-delimited list of minor words that must always be lowercase except for the first word in the string. The JavaScript/CoffeeScript tests will pass undefined when this argument is unused.
+  Example
+  titleCase('a clash of KINGS', 'a an the of') // should return: 'A Clash of Kings'
+  titleCase('THE WIND IN THE WILLOWS', 'The In') // should return: 'The Wind in the Willows'
+  titleCase('the quick brown fox') // should return: 'The Quick Brown Fox'
+*/
+
+// My code
+const wordInString = (s, word) => new RegExp("\\b" + word + "\\b", "i").test(s);
+
+function titleCase(title, minorWords) {
+  if (title.length === 0) return "";
+  return title
+    .split(" ")
+    .map((word, index) => {
+      return index === 0
+        ? word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+        : !!minorWords && wordInString(minorWords, word)
+        ? word.toLowerCase()
+        : word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+    })
+    .join(" ");
+}
+
+// Alternative
+String.prototype.capitalize = function () {
+  return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase();
+};
+
+function titleCase(title, minorWords) {
+  var titleAr = title.toLowerCase().split(" "),
+    minorWordsAr = minorWords ? minorWords.toLowerCase().split(" ") : [];
+
+  return titleAr
+    .map(function (e, i) {
+      return minorWordsAr.indexOf(e) === -1 || i === 0 ? e.capitalize() : e;
+    })
+    .join(" ");
+}
+
+const titleCase = (title, minorWords) => {
+  if (!title) return title;
+
+  let cap = (word) => word[0].toUpperCase() + word.slice(1);
+
+  let minors = (minorWords || "").toLowerCase().split(" ");
+
+  let result = title
+    .toLowerCase()
+    .replace(/\S+/g, (w) => (minors.indexOf(w) === -1 ? cap(w) : w));
+
+  return cap(result);
+};
