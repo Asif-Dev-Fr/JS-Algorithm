aa7e42651ca4b4d4561efa3805c8a21899040851
Asif Kassam Ali <asif.k.dev@gmail.com>
2023-01-17 18:24:41 +0100

    algo added

--- algorithm.js
+++ algorithm.js
@@ -4339,3 +4339,26 @@ function rowSumOddNumbers(n) {
 function rowSumOddNumbers(n) {
   return n ** 3;
 }
+
+/* ---------------------------------------------------------------------------------------------------------------------------*/
+
+/*
+  DESCRIPTION:
+  I have four positive integers, A, B, C and D, where A < B < C < D. The input is a list of the integers A, B, C, D, AxB, BxC, CxD, DxA in some order. Your task is to return the value of D.
+*/
+
+// My code
+function alphabet(ns) {
+  ns.sort((a, b) => a - b);
+  let a = ns[0];
+  let b = ns[1];
+  let AxB = a * b;
+  let c = ns[2] === AxB ? ns[3] : ns[2];
+
+  let BxC = b * c;
+  let CxD = ns[ns.length - 1];
+
+  let d = CxD / c;
+
+  return d;
+}
