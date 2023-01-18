f98bad465bd58fba7cfc2da5e38c941303330ff8
Asif Kassam Ali <asif.k.dev@gmail.com>
2023-01-18 18:45:09 +0100

    algo added

--- algorithm.js
+++ algorithm.js
@@ -4362,3 +4362,48 @@ function alphabet(ns) {
 
   return d;
 }
+
+/* ---------------------------------------------------------------------------------------------------------------------------*/
+
+/*
+  DESCRIPTION:
+  Return a function that will trim a string (the first argument given) if it is longer than the maximum string length (the second argument given). The result should also end with "..."
+
+  These dots at the end also add to the string length.
+
+  So in the above example, trim("Creating kata is fun", 14) should return "Creating ka..."
+
+  If the string is smaller than or equal to 3 characters then the length of the dots is not added to the string length.
+
+  e.g. trim("He", 1) should return "H..."
+
+  If the string is smaller or equal than the maximum string length, then simply return the string with no trimming or dots required.
+
+  e.g. trim("Code Wars is pretty rad", 50) should return "Code Wars is pretty rad"
+*/
+
+// My code
+function trim(str, size) {
+  if (str.length < size || str.length === size) return str;
+
+  let arr = [];
+  str.split("").map((l, index) => {
+    arr.length < size - 3 ? arr.push(l) : "";
+  });
+  if (size < 3) {
+    let i = 0;
+    while (arr.length < size) {
+      arr.push(str[i]);
+      i++;
+    }
+  }
+  arr.push("...");
+  return arr.join("");
+}
+
+// Alternative
+function trim(arr, size) {
+  return arr.length <= size
+    ? arr
+    : arr.slice(0, arr.length > 3 ? size - 3 : size) + "...";
+}
