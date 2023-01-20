238b494e8647ff0e7a9c828e4a0ddb1c90ed2068
Asif Kassam Ali <asif.k.dev@gmail.com>
2023-01-20 18:13:42 +0100

    algo added

--- algorithm.js
+++ algorithm.js
@@ -4430,3 +4430,80 @@ function noSpace(x) {
 }
 
 const noSpace = (x) => x.replace(/ /g, "");
+
+/* ---------------------------------------------------------------------------------------------------------------------------*/
+
+/*
+  DESCRIPTION:
+  Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+  For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1. (81-1-1-81)
+
+  Example #2: An input of 765 will/should return 493625 because 72 is 49, 62 is 36, and 52 is 25. (49-36-35)
+
+  Note: The function accepts an integer and returns an integer.
+
+  Happy Coding!
+*/
+
+// My code
+function squareDigits(num) {
+  num = String(num)
+    .split("")
+    .map((n) => {
+      return Math.pow(n, 2);
+    })
+    .join("");
+  return Number(num);
+}
+
+// Alternative
+function squareDigits(num) {
+  return Number(
+    ("" + num)
+      .split("")
+      .map(function (val) {
+        return val * val;
+      })
+      .join("")
+  );
+}
+
+function squareDigits(num) {
+  return +num
+    .toString()
+    .split("")
+    .map((i) => i * i)
+    .join("");
+}
+
+/* ---------------------------------------------------------------------------------------------------------------------------*/
+
+/*
+  DESCRIPTION:
+  Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the integer's divisors(except for 1 and the number itself), from smallest to largest. If the number is prime return the string '(integer) is prime' (null in C#) (use Either String a in Haskell and Result<Vec<u32>, String> in Rust).
+
+Example:
+divisors(12); // should return [2,3,4,6]
+divisors(25); // should return [5]
+divisors(13); // should return "13 is prime"
+*/
+
+// My code
+function divisors(integer) {
+  let arr = [];
+  for (let i = 2; i < integer; i++) {
+    if (integer % i === 0) {
+      arr.push(i);
+    }
+  }
+  return arr.length > 0 ? arr : integer + " is prime";
+}
+
+// Alternative
+function divisors(integer) {
+  var res = [];
+  for (var i = 2; i <= Math.floor(integer / 2); ++i)
+    if (integer % i == 0) res.push(i);
+  return res.length ? res : integer + " is prime";
+}
