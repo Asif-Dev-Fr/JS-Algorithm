003b0a8811fa8920a70cf33fc4b3005c1bbd4a51
Asif Kassam Ali <asif.k.dev@gmail.com>
2023-01-25 18:34:53 +0100

    algo to finish

--- algorithm.js
+++ algorithm.js
@@ -4531,3 +4531,40 @@ function digital_root(n) {
       }, 0)
   );
 }
+
+/* ---------------------------------------------------------------------------------------------------------------------------*/
+
+/*
+  DESCRIPTION:
+  The snail crawls up the column. During the day it crawls up some distance. During the night she sleeps, so she slides down for some distance (less than crawls up during the day).
+
+  Your function takes three arguments:
+
+  The height of the column (meters)
+  The distance that the snail crawls during the day (meters)
+  The distance that the snail slides down during the night (meters)
+  Calculate number of day when the snail will reach the top of the column.
+*/
+
+// My code
+// https://www.codewars.com/kata/5b93fecd8463745630001d05/train/javascript
+function snail(column, day, night) {
+  let result = 0;
+  let sum = 0;
+  //   while(sum < column) {
+  //       sum += (day - night)
+  //       result++
+  //       console.log(sum, result, column, sum + (day - night))
+  //   }
+
+  for (let i = result; sum < column; result++) {
+    console.log("result", result);
+    sum += day - night;
+    if (sum >= column) {
+      break;
+    }
+    // console.log(sum, result, column, sum + (day - night))
+  }
+
+  return result;
+}
