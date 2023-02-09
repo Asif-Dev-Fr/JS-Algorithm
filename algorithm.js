e4e76674888f928cc581e4ddfb8043a39fe4ac13
Asif Kassam Ali <asif.k.dev@gmail.com>
2023-02-09 18:17:17 +0100

    algo added

--- algorithm.js
+++ algorithm.js
@@ -5143,3 +5143,85 @@ const sequenceSum = (begin, end, step) => {
   }
   return sum;
 };
+
+/* ---------------------------------------------------------------------------------------------------------------------------*/
+
+/*
+  DESCRIPTION:
+  Given a string str, reverse it and omit all non-alphabetic characters.
+
+  Example
+  For str = "krishan", the output should be "nahsirk".
+
+  For str = "ultr53o?n", the output should be "nortlu".
+*/
+
+// My code
+function reverseLetter(str) {
+  return str
+    .split("")
+    .map((l) => {
+      return l.match(/[a-zA-Z]/g);
+    })
+    .reverse()
+    .join("");
+}
+
+// Alternative
+const reverseLetter = (s) =>
+  s
+    .replace(/[^a-z]/gi, "")
+    .split("")
+    .reverse()
+    .join("");
+
+const reverseLetter = (str) => str.match(/[a-z]/g).reverse().join("");
+
+/* ---------------------------------------------------------------------------------------------------------------------------*/
+
+/*
+  DESCRIPTION:
+  In this Kata, you will be given a string that may have mixed uppercase and lowercase letters and your task is to convert that string to either lowercase only or uppercase only based on:
+
+  make as few changes as possible.
+  if the string contains equal number of uppercase and lowercase letters, convert the string to lowercase.
+  For example:
+
+  solve("coDe") = "code". Lowercase characters > uppercase. Change only the "D" to lowercase.
+  solve("CODe") = "CODE". Uppercase characters > lowecase. Change only the "e" to uppercase.
+  solve("coDE") = "code". Upper == lowercase. Change all to lowercase.
+*/
+
+// My code
+function solve(s) {
+  let lowerCase = s.split("").filter((l) => {
+    return l.match(/[a-z]/g);
+  }).length;
+
+  let upperCase = s.split("").filter((l) => {
+    return l.match(/[A-Z]/g);
+  }).length;
+
+  if (lowerCase > upperCase) return s.toLowerCase();
+  else if (lowerCase === upperCase) return s.toLowerCase();
+  else return s.toUpperCase();
+}
+
+function solve(s) {
+  let lowerCase = 0;
+  let upperCase = 0;
+
+  s.split("").map((l) => {
+    return l.match(/[A-Z]/g) ? upperCase++ : lowerCase++;
+  });
+
+  if (lowerCase > upperCase) return s.toLowerCase();
+  else if (lowerCase === upperCase) return s.toLowerCase();
+  else return s.toUpperCase();
+}
+
+// Alternative
+const solve = (s) =>
+  s.replace(/[A-Z]/g, "").length < s.length / 2
+    ? s.toUpperCase()
+    : s.toLowerCase();
