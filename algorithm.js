33439d2185fc81a51eb6a32e702b1e0883624d2c
Asif Kassam Ali <asif.k.dev@gmail.com>
2023-02-14 18:18:18 +0100

    algo added

--- algorithm.js
+++ algorithm.js
@@ -5357,3 +5357,76 @@ function inAscOrder(arr) {
 
 // Alternative
 const inAscOrder = (arr) => arr.join("") === arr.sort((a, b) => a - b).join("");
+
+/* ---------------------------------------------------------------------------------------------------------------------------*/
+
+/*
+  DESCRIPTION:
+  Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+
+  Example:
+  get_char(65)
+  should return: 'A'
+
+*/
+
+function getChar(c) {
+  return String.fromCharCode(c);
+}
+
+/* ---------------------------------------------------------------------------------------------------------------------------*/
+
+/*
+  DESCRIPTION:
+  Take an integer n (n >= 0) and a digit d (0 <= d <= 9) as an integer.
+
+  Square all numbers between 0 and n.
+
+  Count the numbers of digits d used in the writing of all the sqaured number.
+
+  Call nb_dig (or nbDig or ...) the function taking n and d as parameters and returning this count.
+
+  Examples:
+  n = 10, d = 1 
+  the squared number are 0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100
+  We are using the digit 1 in: 1, 16, 81, 100. The total count is then 4.
+
+  nb_dig(25, 1) returns 11 since
+  the squared number that contain the digit 1 are:
+  1, 16, 81, 100, 121, 144, 169, 196, 361, 441.
+  So there are 11 digits 1 for the squares of numbers between 0 and 25.
+  Note that 121 has twice the digit 1.
+*/
+function nbDig(n, d) {
+  let sum = 0;
+  for (let i = 0; i <= n; i++) {
+    let num = String(Math.pow(i, 2)).split("");
+    num.map((n) => {
+      return Number(n) === d ? sum++ : null;
+    });
+  }
+  return sum;
+}
+
+// Alternative
+function nbDig(n, d) {
+  var o = "";
+  for (var i = 0; i <= n; i++) {
+    o += Math.pow(i, 2);
+  }
+  return o.split(d).length - 1;
+}
+
+const nbDig = (n, d) =>
+  Array.from(Array(n + 1), (e, i) => i * i)
+    .join("")
+    .match(new RegExp(d, "g")).length;
+
+function nbDig(n, d) {
+  let count = "";
+  const re = RegExp(d, "g");
+  for (let i = 0; i <= n; i++) {
+    count += i ** 2;
+  }
+  return count.match(re).length;
+}
