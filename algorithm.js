7723a41c0d3ba60452560d4851e177a4aa9de7e6
Asif Kassam Ali <asif.k.dev@gmail.com>
2023-02-21 18:05:22 +0100

    algo added

--- algorithm.js
+++ algorithm.js
@@ -5642,3 +5642,61 @@ function sumDigits(number) {
       return +a + +b;
     }, 0);
 }
+
+/* ---------------------------------------------------------------------------------------------------------------------------*/
+
+/*
+  DESCRIPTION:
+  Your task in this kata is to implement a function that calculates the sum of the integers inside a string. For example, in the string "The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog", the sum of the integers is 3635.
+
+  Note: only positive integers will be tested.
+*/
+
+// My code
+function sumOfIntegersInString(s) {
+  const searchNumber = s.split(/[^0-9]/gi);
+  let sum = 0;
+  for (let i = 0; i < searchNumber.length; i++) {
+    sum += Number(searchNumber[i]);
+  }
+  return sum;
+}
+
+// Alternative
+function sumOfIntegersInString(s) {
+  return (s.match(/\d+/g) || []).reduce((s, n) => s + +n, 0);
+}
+
+/* ---------------------------------------------------------------------------------------------------------------------------*/
+
+/*
+  DESCRIPTION:
+  Write a program that outputs the top n elements from a list.
+
+  Example:
+
+  largest(2, [7,6,5,4,3,2,1])
+  // => [6,7]
+*/
+
+// My code
+function largest(n, array) {
+  return array
+    .sort((a, b) => b - a)
+    .slice(0, n)
+    .sort((a, b) => a - b);
+}
+
+function largest(n, array) {
+  let arr = array.sort((a, b) => b - a).slice(0, n);
+  return arr.sort((a, b) => a - b);
+}
+
+// Alternative
+function largest(n, xs) {
+  return xs
+    .sort(function (a, b) {
+      return a - b;
+    })
+    .slice(xs.length - n);
+}
