feba089bb2d876875c8567619ef6f53eeece9c1e
Asif Kassam Ali <asif.k.dev@gmail.com>
2023-02-27 18:21:20 +0100

    algo added

--- algorithm.js
+++ algorithm.js
@@ -5981,3 +5981,24 @@ const removeDuplicateWords = (s) => {
   const set = new Set(s.split(" "));
   return Array.from(set).join(" ");
 };
+
+/* ---------------------------------------------------------------------------------------------------------------------------*/
+
+/*
+  DESCRIPTION:
+  It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+*/
+
+// My code
+function removeChar(str) {
+  return str.slice(1, -1);
+}
+
+
+// Alternative
+function removeChar(str) {
+  let word = str.split("");
+  word.shift();
+  word.pop();
+  return word.join("");
+}
