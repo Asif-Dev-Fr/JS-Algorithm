a93cd807b71d848b2a86e27dc220124f76348092
Asif Kassam Ali <asif.k.dev@gmail.com>
2023-02-28 17:21:40 +0100

    algo added

--- algorithm.js
+++ algorithm.js
@@ -5986,19 +5986,38 @@ const removeDuplicateWords = (s) => {
 
 /*
   DESCRIPTION:
-  It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+  You will be given an array of objects (hashes in ruby) representing data about developers who have signed up to attend the coding meetup that you are organising for the first time.
+
+  Your task is to return the number of JavaScript developers coming from Europe.
+
+  For example, given the following list:
+
+  var list1 = [
+    { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+    { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+    { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
+    { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+  ];
+  your function should return number 1.
+
+  If, there are no JavaScript developers from Europe then your function should return 0.
+
+  Notes:
+
+  The format of the strings will always be Europe and JavaScript.
+  All data will always be valid and uniform as in the example above.
 */
 
 // My code
-function removeChar(str) {
-  return str.slice(1, -1);
+function countDevelopers(list) {
+  return list.filter((value) => {
+    return value.continent === "Europe" && value.language === "JavaScript";
+  }).length;
 }
 
-
 // Alternative
-function removeChar(str) {
-  let word = str.split("");
-  word.shift();
-  word.pop();
-  return word.join("");
+function countDevelopers(list) {
+  return list.filter(
+    (x) => x.continent == "Europe" && x.language == "JavaScript"
+  ).length;
 }
