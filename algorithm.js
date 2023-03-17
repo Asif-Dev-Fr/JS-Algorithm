9677e6b544297d6e7613e0c1df7e6b0052640bd1
Asif Kassam Ali <asif.k.dev@gmail.com>
2023-03-17 18:13:57 +0100

    algo added

--- algorithm.js
+++ algorithm.js
@@ -6106,3 +6106,45 @@ function roundToNext5(n) {
   while (n % 5 !== 0) n++;
   return n;
 }
+
+/* ---------------------------------------------------------------------------------------------------------------------------*/
+
+/*
+  DESCRIPTION:
+  Grade book
+  Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+
+  Numerical Score	Letter Grade
+  90 <= score <= 100	'A'
+  80 <= score < 90	'B'
+  70 <= score < 80	'C'
+  60 <= score < 70	'D'
+  0 <= score < 60	'F'
+  Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
+*/
+
+// My code
+function getGrade(s1, s2, s3) {
+  let result = (s1 + s2 + s3) / 3;
+
+  switch (true) {
+    case result >= 90 && result <= 100:
+      return "A";
+    case result >= 80 && result < 90:
+      return "B";
+    case result >= 70 && result < 80:
+      return "C";
+    case result >= 60 && result < 70:
+      return "D";
+    case result >= 0 && result < 60:
+      return "F";
+    default:
+      return "";
+  }
+}
+
+// Alternative
+function getGrade(s1, s2, s3) {
+  var s = (s1 + s2 + s3) / 3;
+  return s >= 90 ? "A" : s >= 80 ? "B" : s >= 70 ? "C" : s >= 60 ? "D" : "F";
+}
