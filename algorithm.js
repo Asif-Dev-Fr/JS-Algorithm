29c92358ed60e3944684fec70e207ceabcafded5
Asif Kassam Ali <asif.k.dev@gmail.com>
2023-03-28 17:38:41 +0200

    algo added

--- algorithm.js
+++ algorithm.js
@@ -6271,3 +6271,67 @@ function sum_pairs(ints, s) {
     seen.add(i);
   }
 }
+
+/* ---------------------------------------------------------------------------------------------------------------------------*/
+
+/*
+  DESCRIPTION:
+  Task
+  Given a list of digits, return the smallest number that could be formed from these digits, using the digits only once (ignore duplicates).
+
+  Notes:
+  Only positive integers will be passed to the function (> 0 ), no negatives or zeros.
+  Input >> Output Examples
+  minValue ({1, 3, 1})  ==> return (13)
+  Explanation:
+  (13) is the minimum number could be formed from {1, 3, 1} , Without duplications
+
+  minValue({5, 7, 5, 9, 7})  ==> return (579)
+  Explanation:
+  (579) is the minimum number could be formed from {5, 7, 5, 9, 7} , Without duplications
+
+  minValue({1, 9, 3, 1, 7, 4, 6, 6, 7}) return  ==> (134679)
+  Explanation:
+  (134679) is the minimum number could be formed from {1, 9, 3, 1, 7, 4, 6, 6, 7} , Without duplications
+*/
+
+// My code
+function minValue(values) {
+  let result = [];
+  for (let n of values) {
+    if (!result.includes(n)) result.push(n);
+  }
+  return Number(result.sort((a, b) => a - b).join(""));
+}
+
+// Alternative
+function minValue(values) {
+  let arr = Array.from(new Set(values));
+  return parseInt(arr.sort().join(""));
+}
+
+function minValue(values) {
+  const n = values
+    .filter((e, i, arr) => arr.indexOf(e) === i)
+    .sort()
+    .join("");
+  return Number(n);
+}
+
+/* ---------------------------------------------------------------------------------------------------------------------------*/
+
+/*
+  DESCRIPTION:
+  Given an array of numbers, return a new array of length number containing the last even numbers from the original array (in the same order). The original array will be not empty and will contain at least "number" even numbers.
+
+  For example:
+
+  ([1, 2, 3, 4, 5, 6, 7, 8, 9], 3) => [4, 6, 8]
+  ([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2) => [-8, 26]
+  ([6, -25, 3, 7, 5, 5, 7, -3, 23], 1) => [6]
+*/
+
+// My code
+function evenNumbers(array, number) {
+  return array.filter((n) => n % 2 === 0).slice(-number);
+}
