4f20082c66052fa6f4c7f6ae3ca75611d9260587
Asif Kassam Ali <asif.k.dev@gmail.com>
2023-04-12 18:08:24 +0200

    algo added

--- algorithm.js
+++ algorithm.js
@@ -6430,3 +6430,45 @@ function vowelIndices(word) {
   }
   return arr;
 }
+
+/*---------------------------------------------------------------------------------------------------------------------------*/
+
+/*
+  DESCRIPTION:
+  Given two arrays of strings a1 and a2 return a sorted array r in lexicographical order of the strings of a1 which are substrings of strings of a2.
+
+  Example 1:
+  a1 = ["arp", "live", "strong"]
+
+  a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+  returns ["arp", "live", "strong"]
+
+  Example 2:
+  a1 = ["tarp", "mice", "bull"]
+
+  a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+  returns []
+*/
+
+// My code
+function inArray(array1, array2) {
+  let result = [];
+
+  for (let i = 0; i < array1.length; i++) {
+    for (let j = 0; j < array2.length; j++) {
+      if (array2[j].indexOf(array1[i]) !== -1) {
+        result.push(array1[i]);
+        break;
+      }
+    }
+  }
+
+  return result.sort();
+}
+
+// Alternative
+function inArray(array1, array2) {
+  return array1.filter((a1) => array2.find((a2) => a2.match(a1))).sort();
+}
