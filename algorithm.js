cb6731c000c60a500cbcae5cc7f0b35f4a8b1736
Asif Kassam Ali <asif.k.dev@gmail.com>
2023-04-20 17:47:24 +0200

    algo added

--- algorithm.js
+++ algorithm.js
@@ -6706,6 +6706,7 @@ function DNAStrand(dna) {
 /*---------------------------------------------------------------------------------------------------------------------------*/
 
 /*
+  DESCRIPTION:
   Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
 
   (In this case, all triangles must have surface greater than 0 to be accepted).
@@ -6716,3 +6717,57 @@ function DNAStrand(dna) {
 function isTriangle(a, b, c) {
   return a + b > c && b + c > a && c + a > b;
 }
+
+/*---------------------------------------------------------------------------------------------------------------------------*/
+
+/*
+  DESCRIPTION:
+  You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block for each letter (direction) and you know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.
+
+  Note: you will always receive a valid array containing a random assortment of direction letters ('n', 's', 'e', or 'w' only). It will never give you an empty array (that's not a walk, that's standing still!).
+
+*/
+
+// My code
+function isValidWalk(walk) {
+  let currentPlace = 0;
+  let minute = 0;
+  let coord = {
+    n: 1,
+    s: -1,
+    w: 2,
+    e: -2,
+  };
+  for (let x of walk) {
+    currentPlace += coord[x];
+    minute++;
+  }
+  return currentPlace === 0 && minute === 10;
+}
+
+// Alternative
+function isValidWalk(walk) {
+  var dx = 0;
+  var dy = 0;
+  var dt = walk.length;
+
+  for (var i = 0; i < walk.length; i++) {
+    switch (walk[i]) {
+      case "n":
+        dy--;
+        break;
+      case "s":
+        dy++;
+        break;
+      case "w":
+        dx--;
+        break;
+      case "e":
+        dx++;
+        break;
+    }
+  }
+
+  return dt === 10 && dx === 0 && dy === 0;
+}
+
