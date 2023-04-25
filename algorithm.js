b2464aa0b19e52cf0e5febb3b77ffb9a61beab27
Asif Kassam Ali <asif.k.dev@gmail.com>
2023-04-25 18:26:38 +0200

    algo added

--- algorithm.js
+++ algorithm.js
@@ -6771,3 +6771,77 @@ function isValidWalk(walk) {
   return dt === 10 && dx === 0 && dy === 0;
 }
 
+/*---------------------------------------------------------------------------------------------------------------------------*/
+
+/*
+  DESCRIPTION:
+  You need to create a function, helloWorld, that will return the String Hello, World! without actually using raw strings. This includes quotes, double quotes and template strings. You can, however, use the String constructor and any related functions.
+
+  You cannot use the following:
+
+  "Hello, World!"
+  'Hello, World!'
+  `Hello, World!`
+  Good luck and try to be as creative as possible!
+
+*/
+
+// My code
+function helloWorld() {
+  String.fromCharCode(
+    72,
+    101,
+    108,
+    108,
+    111,
+    44,
+    32,
+    87,
+    111,
+    114,
+    108,
+    100,
+    33
+  );
+}
+
+// Alternative
+function helloWorld() {
+  var d = String.fromCharCode(100);
+  var l = String.fromCharCode(108);
+  var e = String.fromCharCode(101);
+  var r = String.fromCharCode(114);
+  var H = String.fromCharCode(72);
+  var W = String.fromCharCode(87);
+  var o = String.fromCharCode(111);
+  var comma = String.fromCharCode(44);
+  var blank = String.fromCharCode(32);
+  var exclamation = String.fromCharCode(33);
+  return H + e + l + l + o + comma + blank + W + o + r + l + d + exclamation;
+}
+
+
+/*---------------------------------------------------------------------------------------------------------------------------*/
+
+/*
+  DESCRIPTION:
+  Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+
+  Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+
+  Examples:
+
+  Testing: [0, 0, 0, 1] ==> 1
+  Testing: [0, 0, 1, 0] ==> 2
+  Testing: [0, 1, 0, 1] ==> 5
+  Testing: [1, 0, 0, 1] ==> 9
+  Testing: [0, 0, 1, 0] ==> 2
+  Testing: [0, 1, 1, 0] ==> 6
+  Testing: [1, 1, 1, 1] ==> 15
+  Testing: [1, 0, 1, 1] ==> 11
+  However, the arrays can have varying lengths, not just limited to 4.
+
+*/
+
+// My code
+const binaryArrayToNumber = arr => parseInt(arr.join(''), 2);
